89b67648d6f73ac3bb71bdbf9fbe6dad19227642
RuhmUndAnsehen <97001540+RuhmUndAnsehen@users.noreply.github.com>
2023-02-27 23:24:06 +0100

    Fix GPL notice.

--- lib/boombox/amplifier/dsl/engine.rb
+++ lib/boombox/amplifier/dsl/engine.rb
@@ -1,20 +1,19 @@
 # frozen_string_literal: true
 
-#    Financial instrument library for Boombox
-#    Copyright (C) 2021-2023 RuhmUndAnsehen
+#    This file is part of Boombox Amplifier.
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+#    Boombox Amplifier is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by the
+#    Free Software Foundation, either version 3 of the License, or (at your
+#    option) any later version.
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+#    Boombox Amplifier is distributed in the hope that it will be useful, but
+#    WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+#    or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+#    more details.
 #
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#    You should have received a copy of the GNU General Public License along
+#    with Boombox Amplifier. If not, see <https://www.gnu.org/licenses/>.
 
 require 'boombox/amplifier/version'
 
