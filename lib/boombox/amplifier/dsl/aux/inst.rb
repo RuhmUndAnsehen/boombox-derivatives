b2c21550836bc83f3841e19d9ab8fcdc1e4c4e14
RuhmUndAnsehen <97001540+RuhmUndAnsehen@users.noreply.github.com>
2023-04-11 09:30:46 +0200

    Mute Rubocop for a single loc.

--- lib/boombox/amplifier/dsl/aux/inst.rb
+++ lib/boombox/amplifier/dsl/aux/inst.rb
@@ -169,7 +169,9 @@ module Boombox
       end
 
       def raw_value
+        # rubocop:disable Naming/MemoizedInstanceVariableName
         @value ||= []
+        # rubocop:enable Naming/MemoizedInstanceVariableName
       end
 
       def replace(array)
